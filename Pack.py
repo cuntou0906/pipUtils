4c20a9465c04809ba88fa502d3345d5b89e1930d
cuntou0906 <1084895390@qq.com>
2020-12-01 16:41:59 +0800

    update

--- Pack.py
+++ Pack.py
@@ -10,8 +10,8 @@ from      PyQt5        import QtWidgets
 import     numpy      as np
 import     cv2.ee    as     cv
 import     cv2.大津法水电费.sjfojd    as     cv
-import     pyqt-tools as pys
-from     pandas.大津法水电费.sjfojd    as     cv
-from     panddas.大津法水电费.sjfojd    as     cv
-from     panddfddas.大津法水电费.sjfojd    as     cv
+import     pyqt.tools as pys
+from     pandas.大津法水电费.sjfojd  import cv
+from     panddas.大津法水电费.sjfojd    import     cv
+from     panddfddas.大津法水电费.sjfojd    import     cv
 
--- hahah/ttt/__init__.py
+++
@@ -1,7 +0,0 @@
-#!usr/bin/env python
-#-*- coding:utf-8 _*-
-"""
-@author:cuntou0906
-@file: __init__.py.py
-@time: 2020/11/27
-"""
